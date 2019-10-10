d630bf4ae32243bfc0fed8be70d94690da0c287a
Arely Guevara <arelyguevara@Administrators-MacBook-Pro.local>
2019-10-09 23:32:15 -0400

    Updates

---
+++ precache-manifest.d74bb857a6c0bfeb68fd527c0af804f2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a716ac3b597310b00ed8a5452cf4e0",
+    "url": "/react-product-page2/index.html"
+  },
+  {
+    "revision": "e91b80b2d5143d174aa2",
+    "url": "/react-product-page2/static/css/2.8ac39745.chunk.css"
+  },
+  {
+    "revision": "54dfeed3656073afc540",
+    "url": "/react-product-page2/static/css/main.4651c470.chunk.css"
+  },
+  {
+    "revision": "e91b80b2d5143d174aa2",
+    "url": "/react-product-page2/static/js/2.dccbc67c.chunk.js"
+  },
+  {
+    "revision": "54dfeed3656073afc540",
+    "url": "/react-product-page2/static/js/main.1cf32599.chunk.js"
+  },
+  {
+    "revision": "18bde3b4f7a30c529e11",
+    "url": "/react-product-page2/static/js/runtime-main.3b010f1a.js"
+  },
+  {
+    "revision": "b5cecc6819f6333897ee6667697d5ffd",
+    "url": "/react-product-page2/static/media/Shamber.b5cecc68.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-product-page2/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-product-page2/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-product-page2/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-product-page2/static/media/slick.f97e3bbf.svg"
+  }
+]);
