fb334cec1ae46851c95c347a1ae3e02548865520
Arely Guevara <arelyguevara@Administrators-MacBook-Pro.local>
2019-10-09 23:46:54 -0400

    fixed alt tag on images

--- src/Components/ProductPage/Product.js
+++ src/Components/ProductPage/Product.js
@@ -5,7 +5,7 @@ const Product = (props) => {
     return(
         <React.Fragment>
             <div className="grid-item">
-            <img className="grid-image" src={`assets/img/${props.photos}`} alt="{props.name}"/>
+            <img className="grid-image" src={`assets/img/${props.photos}`} alt={props.name}/>
             <p className="grid-item-name">
             { props.name }
             </p>
@@ -21,29 +21,3 @@ const Product = (props) => {
 }
 
 export default Product;
-
-// import React from 'react';
-// import {Button} from 'react-bootstrap';
-
-// //this is the product component
-// const Product = (props) => {
-//     return(
-//         <React.Fragment>
-//             <div className="grid-item">
-//             <img className="grid-image" src={`assets/img/${props.photos}`} alt="{props.name}"/>
-//             <p className="grid-item-name">
-//             { props.name }
-//             </p>
-//             <p className="grid-description">
-//             { props.description}
-//             </p>
-//             <Button className="price">
-//             ${props.price}
-//             </Button>
-            
-//             </div>
-//         </React.Fragment>
-//     );
-// }
-
-// export default Product;
